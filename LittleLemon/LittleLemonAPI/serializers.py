eb5a0ed98ee95166d550845ead18e703aaa196b8
Parsa Estakhri <parsaestakhry03@gmail.com>
2023-11-26 11:22:39 +0330

    added renderers

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -14,6 +14,7 @@ class MenuItemSerializer(serializers.ModelSerializer):
     # to run a method
     price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
     category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = MenuItem
         fields = "__all__"
