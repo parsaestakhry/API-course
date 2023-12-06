55fe76ea37f8f4c285c90221013c78fbb143af24
Parsa Estakhri <parsaestakhry03@gmail.com>
2023-12-06 10:00:09 +0330

    removed JWT

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "djoser",
-    "rest_framework_simplejwt",
-    "rest_framework_simplejwt.token_blacklist",
+    
 ]
 
 MIDDLEWARE = [
